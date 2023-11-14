556742c9cc98b9eecb8e7c989f09ae0519e642a0
LESLIE\lobol <loboleslie2010@gmail.com>
2023-11-14 22:24:17 +1300

    Learning React Components - 14/11/2023

---
+++ taskopedia/src/Header.jsx
@@ -0,0 +1,25 @@
+function MainHeader(){
+    return(
+      <h1 className="heading1">REACT COURSE</h1>
+    );
+  };
+  
+  const subHeaderStyle={
+    color: "blueviolet",
+    backgroundColor: "lightgray"
+  }
+  
+  function SubHeader(){
+    return(
+      <p style={subHeaderStyle}>This will be an exciting course.</p>
+    );
+  };
+  
+  export default function Header(){
+    return(
+    <div>
+      <MainHeader />
+      <SubHeader />
+    </div>
+    );
+  };
---
+++ taskopedia/src/index.js
@@ -0,0 +1,46 @@
+import ReactDOM from "react-dom/client";
+import "./CSS/style.css";
+import Header from "./Header";
+import Students from "./Students";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MainBody(){
+
+   const whatWeWillLearn = "React JS";
+   const lectureCount = 3;
+
+  return(
+<div>
+    <p>
+      In this course, we will learn {whatWeWillLearn} by building TaskOpedia! <br />
+      Total Lecture - {lectureCount}
+    </p>    
+    <ul>
+      <li>Basic Foundation</li>
+      <li>Funtional and Class Components</li>
+    </ul>
+    {/* <div>
+      Enter Task: <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+  </div> */}
+  </div> 
+  );
+};
+
+
+function Footer(){
+  return(
+    <p style={{color: "gray", backgroundColor: "black"}}>Happy Coding!</p>
+  );
+};
+
+root.render(
+  <div>
+    <Header />
+  <MainBody />
+  <Students />
+  <Footer />
+  </div>
+);
+
+
