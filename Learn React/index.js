efaeb8a15e6b91fbf033a89bdfaa1a29935a60b3
LESLIE\lobol <loboleslie2010@gmail.com>
2023-11-14 22:23:19 +1300

    Learning React Fundamentals

---
+++ Learn React/index.js
@@ -0,0 +1,16 @@
+// ReactDOM.render(
+//   <div>
+//     <h1>React Course</h1>
+//     <p>We will learn react in this course</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<div>
+  <h1>React Course</h1>
+  <p>We will learn react in this course</p>
+</div>
+);
+
+
