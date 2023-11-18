c78a10af3a5038f00f23b8f3bbf9bdbe8520ebe7
LESLIE\lobol <loboleslie2010@gmail.com>
2023-11-18 22:08:43 +1300

    Completed Taskopedia

---
+++ taskopedia/src/Layout/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+      <p style={{color: "gray", backgroundColor: "black"
+       ,marginTop:"10x", textAlign: "center"}}>Happy Coding!</p>
+    );
+  };
+
+  export default Footer;
---
+++ taskopedia/src/Layout/Header.jsx
@@ -0,0 +1,32 @@
+import logo from "../images/react.png";
+
+const MainHeader = () => {
+    return(
+      <div className="pt-3 py-1 pl-2" style={{backgroundColor:"black"}}>
+        <img src={logo} style={{ height: "35px", verticalAlign: "top"}}></img>
+        <span className="h2 pt-4 text-white-50">React Course - TaskOPedia</span>
+      </div>
+    );
+  };
+  
+  const subHeaderStyle={
+    color: "blueviolet",
+    backgroundColor: "lightgray"
+  }
+  
+  const SubHeader = () => {
+    return(
+      <p style={subHeaderStyle} className="text-center">This will be an exciting course.</p>
+    );
+  };
+  
+  const Header = () => {
+    return(
+    <div>
+      <MainHeader />
+      <SubHeader />
+    </div>
+    );
+  };
+
+  export default Header;
